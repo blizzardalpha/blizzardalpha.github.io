a58a54de64f12fecb25e9bd3f1ce1825b0971e65
MEXX <157323742+blizzardalpha@users.noreply.github.com>
2024-01-23 17:44:10 +0500

    Update nav.js ALMOST PERFECTED NAVIGATION
    FIXED THE SLIDING BLOB

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,9 +1,12 @@
 navi = document.querySelectorAll(".navitem");
+sect = document.querySelectorAll(".childSection");
+
 
 navi.forEach(navitem =>{
     navitem.addEventListener('click', () =>{
         navi.forEach(innerItem => innerItem.classList.remove('active'));
-
+        sect.forEach(innerItem => innerItem.classList.remove('active'));
         navitem.classList.add('active');
     })
+    
 });
